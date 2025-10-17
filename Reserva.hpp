855260b5ba8a8d7450463a7055111f78684ba64d
Kethelen Damasceno <kethelen_klsd@hotmail.com>
2025-10-17 18:00:48 -0300

    att

---
+++ Reserva.hpp
@@ -0,0 +1,24 @@
+#include <string>
+#ifndef RESERVA_HPP_INCLUDED
+#define RESERVA_HPP_INCLUDED
+
+
+class Reserva{
+    private:
+        string chegada;
+        string partida;
+        string codigo;
+        int dinheiro;
+        
+    public:
+
+
+
+
+};
+
+
+
+
+
+#endif //RESERVA_HPP_INCLUDED
